c658db55b1eee79504580cbae8cf5d67de2c7e9d
asorokin <a40in@inbox.ru>
2020-07-16 11:38:25 +0300

    test with js

--- gojs/main.go
+++ gojs/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-
 	"github.com/gopherjs/gopherjs/js"
 )
 
-func main(){
+func main() {
 	doc := js.Global.Get("document")
 	println("Hello, browser console!", doc)
+}
+
+func mySearch() {
+	doc := js.Global.Get("document")
+	input := doc.Get("myInput")
+	println("myInput=", input)
 }
